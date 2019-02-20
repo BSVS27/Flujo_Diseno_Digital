2bf026c380d0e70c0228a1b528893a86c8de7c7c
Edgar Solera <esolera@zener.estudiantes.ie.tec.ac.cr>
2019-02-20 10:58:36 -0600

    Lista la sintesis

--- front_end/scripts/analyze_rtl.tcl
+++ front_end/scripts/analyze_rtl.tcl
@@ -13,57 +13,47 @@
 # Revision:	31/07/2018 (ACR)
 #
 ##################################################################################
-
-# Analisis de los modulos/dependencias del disenno. Lluego se elabora el modulo principal que corresponde al  
-# disenno en la variable $TOP_MODULE del script user_setup.tcl.
-
-# En caso de estar corriendo en el modo topografico, se debe crear una biblioteca MW.
 #------------------------------------------------------------------------------------
-
+# En el modo topográfico es necesa
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
+#Activa tambien el mapeo del saif, el mismo debe hacer antes de llamar al diseño para 
+# este preparado de mapear los nombre.
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
 if {[shell_is_in_topographical_mode]} {
-	# Se abre la biblioteca de trabajo Milkyway. En caso de que la misma no exista se crea.
 	source -echo -verbose "$PROY_HOME_SYN/scripts/crear_mw.tcl";
+	saif_map -start; 	
 }
-
-# El siguiente proceso implementa un algoritmo recursivo el cual genera 2 listas, una para todos los directorios
-# que se encuentran en la ruta de archivos fuente y otra para los archivos verilog en dichos directorios, con la
-# segunda lista, se analizan los archivos fuente. Si el directorio base no contiene mas directorios, se leen los
-# archivos fuente desde este.
-# El readfile se genera con el gui para generar la lista completa
 #------------------------------------------------------------------------------------
-
-
+#------------------------------------------------------------------------------------
+# El proximo comando lee los archivos verilog para abrirlos en la herramienta.
+#Para ver los diseños enlistados se utiliza el comando list_designs.
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
 read_file -format sverilog {/mnt/vol_NFS_Zener/WD_ESPEC/moviedo/GDS/nuevo/ALU_Phy/front_end/source/top.sv /mnt/vol_NFS_Zener/WD_ESPEC/moviedo/GDS/nuevo/ALU_Phy/front_end/source/ALU_2.sv /mnt/vol_NFS_Zener/WD_ESPEC/moviedo/GDS/nuevo/ALU_Phy/front_end/source/Barrel_Shifter.sv /mnt/vol_NFS_Zener/WD_ESPEC/moviedo/GDS/nuevo/ALU_Phy/front_end/source/csk_bloque.sv /mnt/vol_NFS_Zener/WD_ESPEC/moviedo/GDS/nuevo/ALU_Phy/front_end/source/CSK_sin_mux.sv};
-
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
+# Analiza el HDL para ver su estado se le indica la libreria con la que se trabajara (-library)
+# tambien se le especifica el formato del HDL (-format)
+# Al final se le da el nombre del que se analizara que usualmente es el top design.
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
 analyze -library WORK -format sverilog $TOP_FILE > reports/analyze.txt;
-#Despues de este comando se puede poner list_designs 
-
-# Este script hcho por RCG carga recursivamente todos los archivos .v en el directorio "source". Hay que probarlo (ACR)
-
-#set directorios [ls -d "$SOURCE_HOME"];					# Lista 1: Directorios Fuente.
-# -------------------------------------------------		  Ciclo 1: indexacion de directorios
-#if {[string trimleft $directorios] != "$SOURCE_HOME"} {
-#   	foreach i $directorios {			
-#		set archivos [ls "$i/*.*v"]	;					# Lista 2: Archivos fuente verilog/system verilog.
-#--------------------------------------------------		  Ciclo 2: indexacion de archivos.
-#		foreach n $archivos {			
-#	        analyze -format sverilog -library WORK "$n";	# Analisis de los archivos fuente.
-#	    };												# Fin de Ciclo 2.
-#	};													# Fin de Ciclo 1.
-#} else {
-#	set archivos [ls "$SOURCE_HOME/*.*v"];				# Lista 2: Archivos fuente verilog/system verilog.
-#--------------------------------------------------		  Ciclo 2: indexacion de archivos.
-#	foreach n $archivos {
-#		analyze -format sverilog -library WORK "$n";		# Analisis de los archivos fuente.
-#	};
-#}
-
-# Elaboracion del modulo principal del disenno
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
+# Construye el diseño que se le indique.
+# Con -architecture se le indica el formato que en este caso se llama sverilog.
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
 elaborate $TOP_MODULE -architecture verilog -library WORK > reports/elaborate.txt;
-
-#set current_design $TOP_MODULE;
-
-# Definimos las direcciones de enrutamiento p
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
+# Los proximos comandos son para el modo topografico que hacen un prelayout
+#Le dicen a la herramienta cuales metales son preferidos para lineas veritcales 
+# y cuales otros para horizontales
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
 if {[shell_is_in_topographical_mode]} {
 set_preferred_routing_direction -layers {MET1 MET3 METTP} -direction horizontal;
 set_preferred_routing_direction -layers {MET2 MET4 METTPL} -direction vertical;
--- front_end/scripts/dc_syn.tcl
+++ front_end/scripts/dc_syn.tcl
@@ -13,116 +13,99 @@
 # Revision:	30-07-18 ACR
 #
 ###################################################################################
-
 puts "RM-Info: Running script [info script]\n"
-
-
-
+#--------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
 # Se suprimen algunos mensajes de alarmas los cuales se considera, pueden dejarse pasar.
-# para ver mas detalles sobre los mensajes suprimidos, se recomienda ejecutar el comando:
-# man <CODIGO>
-
+#--------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
 suppress_message {VER-130 LINT-1 LINT-28 LINT-29 LINT-31 LINT-33 LINT-52 OPT-112 TIM-134\
 				 PWR-6 PWR-410 PWR-428 PWR-412 UID-401};
-
-#------------------------------------------------------------------------------
-# Cargar los archivos fuente y elaborar el disenno
-#------------------------------------------------------------------------------
-#source -verbose "$PROY_HOME/scripts/analyze_rtl.tcl";
-
-# Vincular el disenno
-link > reports/link.txt; # Vincular los diseños
-
-# Revisar el disenno
+#--------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
+# El siguiente comando linkea los verilog entre sí, muchos de ellos se referencian entre ellos.
+#--------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
+link > reports/link.txt; 
+#--------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
+# check_design revisa la consistencia del diseño.
+#--------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
 check_design > reports/check_dsgn.txt;
-# set_ungroup [get_cells {u12 u13 u14 u15 u16}] false; 
-# Ignorar, el comando anterior, se uso a modo de exploracion para no romper la jerarquia.
-# Cosa que se puede hacer con un interruptor del comando compile_ultra
-
+#--------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
+# Escribe el netlist o esquemático con el formato deseado en algun espacio indicado de la memoria.
+#--------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
 write -hierarchy -format ddc -output "$PROY_HOME_SYN/db/$DESIGN_NAME\_pre_compile.ddc";
-# La ruta anterior debe existir!
-
-#------------------------------------------------------------------------------
-# Cargar las restricciones de disenno
-#------------------------------------------------------------------------------
-#En esta parte se correran los constraints 
-#------------------------------------------------------------------------------
-
+#--------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
+# Los dos siguientes comandos crean todas la variables y restricciones que la herramienta debe 
+#considerar al ejecutar la síntesis. El segundo las propaga por el diseño( las ejecuta)
+#--------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
 source -verbose -echo "$PROY_HOME_SYN/scripts/$DESIGN_NAME\_constraints.tcl";
 propagate_constraints;
-
-
-#------------------------------------------------------------------------------
-#				Da permisos de poner buffer donde los necesite
-#------------------------------------------------------------------------------
-#set_fix_multiple_port_nets -feedthroughs -buffer_constants
-
-#------------------------------------------------------------------------------
-#				Activar el analisis del factor de actividad
-#------------------------------------------------------------------------------
-# En esta parte se selecciona el tipo de analsis para estimación de potencia que se va hacer
-# si se utiliza topográfico el analisis se hara por saif file.
-#------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
+#Le inidica que se va hacer una estimacion de portencia durante el compile_ultra o
+#compile_ultra -incremental solo funciona con modo topográfico.
+#--------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
 if {[shell_is_in_topographical_mode]} {
-	saif_map -start; 			
 	set_power_prediction; 		
 } else {
 	propagate_switching_activity; 
 }
-
+#------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
 #								Compilacion
+#           Parte más importante es donde se realiza la sintesis
 #------------------------------------------------------------------------------
-#Compile ultra sintetiza el modulo para llevarlo a nivel de compuertas.
 #------------------------------------------------------------------------------
-compile_ultra -no_auto_ungroup -exact_map > reports/compile.txt; 
-
-
+compile_ultra -no_autoungroup -exact_map > reports/compile.txt; 
 
 #------------------------------------------------------------------------------
 #								Lectura saif
 #------------------------------------------------------------------------------
-# Lectura del saif, unicamente funcional en el modo topográfico
-# read saif:Esta instruccion lee el saif 
-#instance_name: Buscan la instancia dentro del saif.
-#target_instance: busca la instancia dentro del diseño para matchearla con el saif.
+#En esta parte se realiza la lectura del saif, si se desea se le puede dar en 
+# un comprimido tgz. Con -auto_map_names se le indica que infiera aquellos nodos
+#no encontrados.
 #-------------------------------------------------------------------------------
 if {[shell_is_in_topographical_mode]} {
 	read_saif -input "$PROY_HOME/front_end/source/$DESIGN_NAME.saif" \
  -instance_name $TEST_INST_NAME/inst_top -auto_map_names;
 }
-
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+# Declara las variables de tercer estados como wire en ves de tri.
+#------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 set verilogout_no_tri true
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+# Cambia los nombres de los puertos dentro del diseño.
+#------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 change_names -hierarchy -rules verilog 
-
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+# Se generan y guardan los reportes de potencia, area, timing entre otros.
 #------------------------------------------------------------------------------
-#						Insersion de los pads de I/O
-#------------------------------------------------------------------------------
-
-#source -verbose -echo $PROY_HOME/front_end/scripts/pad_insertion.tcl;
-
-#------------------------------------------------------------------------------
-# 							Generacion de Reportes
-#------------------------------------------------------------------------------
-
-# Las rutas de los archivos de salida deben existir
-
+#-------------------------------------------------------------------------------
 report_power -analysis_effort high > "$PROY_HOME_SYN/reports/$DESIGN_NAME\_syn_power.txt";
 report_area >  "$PROY_HOME_SYN/reports/$DESIGN_NAME\_syn_area.txt";
 report_qor > "$PROY_HOME_SYN/reports/$DESIGN_NAME\_syn_qor.txt";
 report_timing > "$PROY_HOME_SYN/reports/$DESIGN_NAME\_syn_timing.txt";
 report_port > "$PROY_HOME_SYN/reports/$DESIGN_NAME\_syn_port.txt";
-
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+# Se Guardan los archivos de salida.
 #------------------------------------------------------------------------------
-# 						Generacion de archivos de salida
-#------------------------------------------------------------------------------
-# Guardamos un formato ddc para leerlo mas rapido de vuelta en el ICC
+#-------------------------------------------------------------------------------
 write -hierarchy -format ddc -output "$TOP_FILE_DDC";
 write -format verilog -hierarchy -output "$TOP_FILE_SYN";
-
-#Guardamos info de restricciones para leerlo en ICC
 write_sdc "$TOP_FILE_SDC";
-
-#Guardamos info de temporizado en sdf para simulaciones 
 write_sdf "$TOP_FILE_SDF";
 puts "RM-Info: Completed script [info script]\n";
