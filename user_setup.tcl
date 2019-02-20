726b661accf458d2d03f4b0e69d6eb8d2d3e9904
Edgar Solera <esolera@zener.estudiantes.ie.tec.ac.cr>
2019-02-20 11:05:56 -0600

    Cambio en el user

--- user_setup.tcl
+++ user_setup.tcl
@@ -21,37 +21,32 @@ puts "RM-Info: Running script [info script]\n"
 ##########################################################################################
 ## Library Setup Variables
 ###########################################################################################
-
-###
-# Para las siguientes variables, use un espacio en blanco para separar los nombres que ingresa.
-###
-
-# Directorio raiz del arbol de directorios de la tecnologia
-
-# Ingrese la direccion absoluta al directorio base del proyecto
-# Primero las variables generales
-# Ejemplo: set PROY_HOME "/mnt/vol_NFS_Zener/WD_ESPEC/$user_name/repositorios/TEC_RISCV/Integration";
-set PROY_HOME "/mnt/vol_NFS_Zener/WD_ESPEC/esolera/Tutorial_synopsys/nuevo/Flujo_Diseno_Digital"; 
+#Variables de dirección de la carpeta del proyecto
+############################################################################################
+set PROY_HOME "../../Flujo_Diseno_Digital"; 
 set PROY_HOME_SYN "$PROY_HOME/front_end";		
-set DESIGN_NAME "alu";	# Ingrese el nombre del proyecto. Ejemplo "ac97";
+set DESIGN_NAME "alu";	
 set SOURCE_HOME_SYN "$PROY_HOME_SYN/source";
 set TOP_FILE "$SOURCE_HOME_SYN/top.sv";
 set TOP_MODULE "top";
 set TEST_INST_NAME "test_top";
 set UUT_INST_NAME "u0";
-
-#VAriables para el ICC
+##########################################################################################
+## Variables para el ICC
+###########################################################################################
 set PROY_HOME_PHY "$PROY_HOME/back_end";
 set SOURCE_HOME_PHY "$PROY_HOME_PHY/source";
 set DB_HOME_PHY "$PROY_HOME_PHY/db"
-
+##########################################################################################
 ## El <design>.ddc, <design>.v y <design>.sdc los traemos directamente de las carpetas de sintesis logica
+##########################################################################################
 set TOP_FILE_SYN "$SOURCE_HOME_SYN/$DESIGN_NAME\_syn.v";
 set TOP_FILE_DDC "$PROY_HOME_SYN/db/$DESIGN_NAME.ddc";
 set TOP_FILE_SDC "$PROY_HOME_SYN/db/$DESIGN_NAME\_syn.sdc";
 set TOP_FILE_SDF "$PROY_HOME_SYN/db/$DESIGN_NAME\_syn.sdf";
-
+##########################################################################################
 ## Archivos de diseno fisico final
+##########################################################################################
 set TOP_FILE_PHY "$SOURCE_HOME_PHY/$DESIGN_NAME\_phy.v";
 set TOP_FILE_PHY_DDC "$DB_HOME_PHY/$DESIGN_NAME.ddc";
 set TOP_FILE_PHY_SDC "$DB_HOME_PHY/$DESIGN_NAME\_phy.sdc";
@@ -59,10 +54,10 @@ set TOP_FILE_PHY_SDF "$DB_HOME_PHY/$DESIGN_NAME\_phy.sdf";
 set TOP_FILE_PHY_SPEF "$DB_HOME_PHY/$DESIGN_NAME\_phy.spef";
 set TOP_FILE_PHY_DEF "$DB_HOME_PHY/$DESIGN_NAME\_phy.def";
 set TOP_FILE_GDS "$DB_HOME_PHY/$DESIGN_NAME\_phy.gds";
+##########################################################################################
 #Vamos a colocar los archivos de mapee MW-gds y de reglas en el directorio donde correra el ICV
-#set TECH_GDS_MAP_FILE "$PROY_HOME_PHY/db/signoff_drc_run/xh018_out.map"; #Archivo mapeo MW GDS
-set TECH_GDS_MAP_FILE "$TECH_ROOT/xh018/synopsys/v6_3/techMW/v6_3_1_2/xh018-synopsys-techMW-v6_3_1_2/xh018_out.map"; #Archivo mapeo MW GDS
-#set TECH_RUNSET_FILE "$PROY_HOME_PHY/db/signoff_drc_run/xh018_1143_DRC_MET4_METMID_METTHK.rs" ;#Archivo reglas proceso ICV
+##########################################################################################
+set TECH_GDS_MAP_FILE "$TECH_ROOT/xh018/synopsys/v6_3/techMW/v6_3_1_2/xh018-synopsys-techMW-v6_3_1_2/xh018_out.map";
 set TECH_RUNSET_FILE "$TECH_ROOT/xh018/synopsys/v7_0/ICValidator/v7_0_3/xh018_1143_DRC_MET4_METMID_METTHK.rs" 
 set TECH_RUNSET_DIR "$TECH_ROOT/xh018/synopsys/v7_0/ICValidator/v7_0_3" 
 
